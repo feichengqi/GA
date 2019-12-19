f34666ce5ad6a36a958f63aa855d8075f995f9ff
Pui-Yee Allyson Ling <allysonling18@gmail.com>
2019-12-19 00:52:38 -0800

    Final report + synthetic data ex

--- syntheticdata.R
+++ syntheticdata.R
@@ -7,7 +7,7 @@ gen_linearcombo = function(x){
   
   # Make sure we get a non trivial linear combination
   while(sum(keep) < 1){
-    keep = rlogical(ncol(x))
+    keep = ga_rlogical(ncol(x))
   }
   
   betas = rnorm(sum(keep))
@@ -21,11 +21,12 @@ gen_linearcombo = function(x){
 }
 
 # Synthetic dataset creation
-n = 2000
-x = matrix(rnorm(n*5), n, 5)
-linearcombos = replicate(15, gen_linearcombo(x))
-probes = matrix(rnorm(n*80, 0, 5), n, 80)
-betas = rnorm(5)
-y = rnorm(n, betas[1]*x + betas[2]*x +
-             betas[3]*x + betas[4]*x + betas[5])
+n = 500
+x = matrix(rnorm(n*3), n, 3)
+linearcombos = replicate(7, gen_linearcombo(x))
+probes = matrix(rnorm(n*10, 0, 5), n, 10)
+betas = rnorm(3)
+y = rnorm(n, betas[1]*x + betas[2]*x + betas[3]*x)
 x = cbind(x,linearcombos,probes)
+x = as.data.frame(x)
+pop = ga_compute(dim = ncol(x), p = nrow(x), t = 100, selection_method = 'score', partial_update = FALSE, data = x, fitness = AIC, func = lm, response = y, min = TRUE)
